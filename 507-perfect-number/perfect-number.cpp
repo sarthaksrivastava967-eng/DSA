87251e7f8cf91c326b27c9f69580574b207d06c9
sarthaksrivastava967-eng <sarthaksrivastava967@gmail.com>
2026-09-10 21:34:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (21.34%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -3,9 +3,7 @@ public:
     bool checkPerfectNumber(int num) {
         if (num <= 1)
             return false;
-
         int sum = 1;
-
         for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) {
                 sum += i;
